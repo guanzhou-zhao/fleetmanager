d9ff2e006d87df356fdf3e48d9cfc083afe3711f
guanzhou zhao <benzhao2866@gmail.com>
2024-01-09 05:34:44 +1300

    add flex's first sample

---
+++ Flex.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {StyleSheet, View} from "react-native"
+
+const Flex = () => {
+    return (
+        <View style={[
+            styles.container,
+            {
+                flexDirection: 'coloum',
+            }
+        ]}>
+            <View style={{
+                flex: 1,
+                backgroundColor: 'red',
+            }}/>
+            <View style={{
+                flex: 2,
+                backgroundColor: 'white',
+            }}/>
+            <View style={{
+                flex: 3,
+                backgroundColor: 'blue',
+            }}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 20
+    },
+});
+
+export default Flex;
