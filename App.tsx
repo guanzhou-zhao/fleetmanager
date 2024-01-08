74d761a559110617d56bb3991d79fef3f72ac56c
guanzhou zhao <benzhao2866@gmail.com>
2024-01-09 11:15:31 +1300

    FlexDirectionBasics.js works learned to use array destructuring to get values from useState use 'flex:1' on outter tag to use all the space on screen to automatically return, don't use curly braces after arrow function 'true && something' gives out something 'flexWrap: 'wrap' ' makes sure descendant elements don't go overflow

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import Flex from './Flex';
+import FlexDirectionBasics from './FlexDirectionBasics'
 import {
   SafeAreaView,
   ScrollView,
@@ -61,39 +63,40 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  // return <Flex />;
+  return <FlexDirectionBasics />;
+  // return (
+  //   <SafeAreaView style={backgroundStyle}>
+  //     <StatusBar
+  //       barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+  //       backgroundColor={backgroundStyle.backgroundColor}
+  //     />
+  //     <ScrollView
+  //       contentInsetAdjustmentBehavior="automatic"
+  //       style={backgroundStyle}>
+  //       <Header />
+  //       <View
+  //         style={{
+  //           backgroundColor: isDarkMode ? Colors.black : Colors.white,
+  //         }}>
+  //         <Section title="Step One hello world! Max Wen ">
+  //           Edit <Text style={styles.highlight}>App.tsx</Text> to <Text style={styles.highlight}>change</Text> this
+  //           screen and then come back to see your edits.
+  //         </Section>
+  //         <Section title="See Your Changes">
+  //           <ReloadInstructions />
+  //         </Section>
+  //         <Section title="Debug">
+  //           <DebugInstructions />
+  //         </Section>
+  //         <Section title="Learn More">
+  //           Read the docs to discover what to do next:
+  //         </Section>
+  //         <LearnMoreLinks />
+  //       </View>
+  //     </ScrollView>
+  //   </SafeAreaView>
+  // );
 }
 
 const styles = StyleSheet.create({
--- FlexDirectionBasics.js
+++ FlexDirectionBasics.js
@@ -3,32 +3,17 @@ import {StyleSheet, View, Text, TouchableOpacity } from "react-native"
 import { useState } from "react";
 
 const FlexDirectionBasics = () => {
-    const {flexDirection, setFlexDirection} = useState('column');
+    // Use array destructuring assignment, not Object destructuring
+    const [flexDirection, setFlexDirection] = useState('row');
     return (
         <FlexControlLayout
             label='flexDirection'
             values={['column', 'row', 'column-reverse', 'row-reverse']}
             selectedValue={flexDirection}
-            setSelectedValue={setFlexDirection}
-        >
-            <View style={[
-                    styles.box,
-                    {
-                        backgroundColor: 'powderblue'
-                    }
-                ]}/>
-            <View style={[
-                    styles.box,
-                    {
-                        backgroundColor: 'skyblue'
-                    }
-                ]}/>
-            <View style={[
-                    styles.box,
-                    {
-                        backgroundColor: 'steelblue'
-                    }
-                ]}/>
+            setSelectedValue={setFlexDirection}>
+            <View style={[styles.box, {backgroundColor: 'powderblue'}]}/>
+            <View style={[styles.box, {backgroundColor: 'skyblue'}]}/>
+            <View style={[styles.box, {backgroundColor: 'steelblue'}]}/>
         </FlexControlLayout>
     )
 }
@@ -39,36 +24,37 @@ const FlexControlLayout = ({
     values,
     selectedValue,
     setSelectedValue
-}) => {
-    return (
-        <View style={[{marginTop: 20}]}>
-            <Text style={styles.lable}>{label}</Text>
-            <View style={styles.buttonContainer}>
-                {
-                    values.map(value => {
-                        <TouchableOpacity 
-                            key={value}
-                            onPress={()=>setSelectedValue(value)}
-                            style={[
-                                styles.button,
-                                value === selectedValue && styles.selected
-                            ]}
-                        >
-                            <Text style={[
-                                styles.buttonLabel
-                            ]}>
-                                {value}
-                            </Text>
-                        </TouchableOpacity>
-                    })
-                }
-            </View>
-            <View style={[styles.container, {[label]: selectedValue}]}>
-                {children}
-            </View>
+}) => (
+    // the style 'flex:1' in the outter tag will make sure the contents takes whole screen space
+    <View style={{padding: 20, flex:1}}>
+        <Text style={styles.lable}>{label}</Text>
+        <View style={styles.buttonContainer}>
+            {/** In order to automaticly return in arrow method, can't write curly braces*/}
+            {values.map(value => (
+                <TouchableOpacity 
+                    key={value}
+                    onPress={()=>setSelectedValue(value)}
+                    style={
+                        // 'true && something' gives out something
+                        [
+                        styles.button,
+                        value === selectedValue && styles.selectedButton
+                    ]}
+                >
+                    <Text style={[
+                        styles.buttonLabel,
+                        value === selectedValue && styles.selectedLabel
+                    ]}>
+                        {value}
+                    </Text>
+                </TouchableOpacity>
+            ))}
         </View>
-    )
-}
+        <View style={[styles.boxContainer, {[label]: selectedValue}]}>
+            {children}
+        </View>
+    </View>
+)
 
 const styles = StyleSheet.create({
     lable: {
@@ -79,32 +65,34 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
     },
-    container: {
+    boxContainer: {
         marginTop: 8,
         flex: 1,
         backgroundColor: 'aliceblue'
     },
-    selected: {
-        color: 'white',
-        backgroundColor: 'coral'
+    selectedButton: {
+        backgroundColor: 'coral',
     },
     button: {
         marginHorizontal: '1%',
         minWidth: '48%',
         marginBottom: 6,
-        paddingHorizontal: 8,
         paddingVertical: 6,
+        paddingHorizontal: 8,
         borderRadius: 4,
         backgroundColor: 'oldlace',
-        textAlign: 'center',
-        alignSelf: 'flex-start'
     },
     buttonContainer: {
         flexDirection: 'row',
-        flexWrap: 'wrap',
+        flexWrap: 'wrap'
     },
     buttonLabel: {
-        
+        fontSize: 20,
+        color: 'coral',
+        fontWeight: '500',
+    },
+    selectedLabel: {
+        color: 'white'
     }
 })
 
